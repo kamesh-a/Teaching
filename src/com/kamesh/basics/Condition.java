d97e858fde7795befe34279759363635d4c7d96c
Kamesh <kamesh.arumugam@a-cti.com>
2016-10-23 18:20:14 +0530

    test

--- src/com/kamesh/basics/Condition.java
+++ src/com/kamesh/basics/Condition.java
@@ -2,22 +2,30 @@ package com.kamesh.basics;
 
 public class Condition {
 	public static void main(String[] args) {
-
-		/**
-		 * 
-		 * Data types :
-		 * 
-		 * byte int long float double char boolean
-		 * 
-		 * 
-		 * 
-		 */
-		int a = 10;
-		int b = 20;
-		int c = a + b;
-		if (false) {
-			System.out.println("Yes we are inside the program hurray, is it compiling again");
+		// 10 == 10
+		if (10 == 10) {
+			System.out.println("print");
 		}
 
+		if (0 != 0) {
+			System.out.println("yes mulitple if");
+		}
+
+		// if else
+
+		if (false) {
+			System.out.println("if");
+		} else {
+			System.out.println("else");
+		}
+		// if elseIf else
+
+		if (false) {
+			System.out.println("1");
+		} else if (false) {
+			System.out.println("2");
+		} else {
+			System.out.println("3");
+		}
 	}
 }
