e3374d9d7bf6a63346464ec9e4936e8284bc26d4
Kamesh <kamesh.arumugam@a-cti.com>
2016-10-23 18:02:42 +0530

    Initial commit

---
+++ src/com/kamesh/basics/Condition.java
@@ -0,0 +1,23 @@
+package com.kamesh.basics;
+
+public class Condition {
+	public static void main(String[] args) {
+
+		/**
+		 * 
+		 * Data types :
+		 * 
+		 * byte int long float double char boolean
+		 * 
+		 * 
+		 * 
+		 */
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+		if (false) {
+			System.out.println("Yes we are inside the program hurray, is it compiling again");
+		}
+
+	}
+}
---
+++ src/com/kamesh/runner/Main.java
@@ -0,0 +1,7 @@
+package com.kamesh.runner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
